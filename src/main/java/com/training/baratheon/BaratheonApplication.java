66ae0f45b47284f9b2af9c89dbd105c862c2114e
bapierre <pierre.baptiste@outlook.com>
2023-04-27 20:40:37 +0200

    Spring boot initialzr

---
+++ src/main/java/com/training/baratheon/BaratheonApplication.java
@@ -0,0 +1,13 @@
+package com.training.baratheon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaratheonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaratheonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/training/baratheon/BaratheonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.baratheon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaratheonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
